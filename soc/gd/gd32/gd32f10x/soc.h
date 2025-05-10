0bb15fb3908c5b25a2c71e07c4e0903f43038f6c
liangyq <liangyongqincn@gmail.com>
2025-05-10 11:14:15 +0800

    add soc gd32f10x: gpio && uart finished

---
+++ soc/gd/gd32/gd32f10x/soc.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2025 liangyq <liangyongqincn@gmail.com>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+ #ifndef _SOC_ARM_GIGADEVICE_GD32F10X_SOC_H_
+ #define _SOC_ARM_GIGADEVICE_GD32F10X_SOC_H_
+ 
+ #ifndef _ASMLANGUAGE
+ 
+ #include <gd32f10x.h>
+ 
+ #endif /* _ASMLANGUAGE */
+ 
+ #endif /* _SOC_ARM_GIGADEVICE_GD32F10X_SOC_H_ */
+ 
